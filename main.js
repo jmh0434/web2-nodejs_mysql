0b2812894d68e4ccad02b44817ba090e68a65be6
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-07-13 21:59:18 +0900

    MySQL로 상세보기 구현

--- main.js
+++ main.js
@@ -21,17 +21,6 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        // fs.readdir('./data', function(error, filelist){
-        //   var title = 'Welcome';
-        //   var description = 'Hello, Node.js';
-        //   var list = template.list(filelist);
-        //   var html = template.HTML(title, list,
-        //     `<h2>${title}</h2>${description}`,
-        //     `<a href="/create">create</a>`
-        //   );
-        //   response.writeHead(200);
-        //   response.end(html);
-        // });
         db.query(`SELECT * FROM topic`, function(error, topics){
           var title = "Welcome";
           var description = "Hello, Node.js";
@@ -46,27 +35,32 @@ var app = http.createServer(function(request,response){
           response.end(html);
         });
       } else {
-        fs.readdir('./data', function(error, filelist){
-          var filteredId = path.parse(queryData.id).base;
-          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-            var title = queryData.id;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-              allowedTags:['h1']
-            });
-            var list = template.list(filelist);
-            var html = template.HTML(sanitizedTitle, list,
-              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-              ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
+        db.query(`SELECT * FROM topic`, function(error, topics){
+          if(error) {
+            throw error;
+          }
+          db.query(`SELECT * FROM topic WHERE ID=?`, [queryData.id], function(error2, topic){
+            if(error2) {
+              throw error2;
+            }
+            var title = topic[0].title;
+            var description = topic[0].description;
+            var list = template.list(topics);
+            var html = template.HTML(
+              title,
+              list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a>
+                <a href="/update?id=${queryData.id}">update</a>
                 <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
+                  <input type="hidden" name="id" value="${queryData.id}">
                   <input type="submit" value="delete">
                 </form>`
+              
             );
             response.writeHead(200);
             response.end(html);
-          });
+          })
         });
       }
     } else if(pathname === '/create'){
