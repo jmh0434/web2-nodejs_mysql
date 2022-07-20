36cd927426c8e7f28cbfe4bcea8d91b3cc5bc583
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-07-20 22:04:48 +0900

    MySQL로 상세보기, 글 생성, 수정, 삭제 구현

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var template = require("./lib/template.js");
 var path = require("path");
 var sanitizeHtml = require("sanitize-html");
 var mysql = require("mysql");
-var db = mysql.createConnection({
+var db = mysql.createConnection({ // mysql을 연결하기 위한 객체
   host: "localhost",
   user: "root",
   password: "wjdalsgur1",
@@ -18,9 +18,9 @@ var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-  if (pathname === "/") {
-    if (queryData.id === undefined) {
-      db.query(`SELECT * FROM topic`, function (error, topics) {
+  if (pathname === "/") { // 차상위 루트라면
+    if (queryData.id === undefined) { // queryData가 존재하지 않는다면, 즉 메인 홈페이지라면
+      db.query(`SELECT * FROM topic`, function (error, topics) { // topic 테이블의 데이터를 읽어와서 화면에 출력
         var title = "Welcome";
         var description = "Hello, Node.js";
         var list = template.list(topics);
@@ -33,12 +33,12 @@ var app = http.createServer(function (request, response) {
         response.writeHead(200);
         response.end(html);
       });
-    } else {
-      db.query(`SELECT * FROM topic`, function (error, topics) {
+    } else { // 그렇지 않다면, 즉 어떠한 게시물을 클릭했을 때
+      db.query(`SELECT * FROM topic`, function (error, topics) { // 우선 위에 전체 리스트를 띄워주고
         if (error) {
           throw error;
         }
-        db.query(
+        db.query( // 선택한 항목의 데이터를 출력한다
           `SELECT * FROM topic WHERE id=?`,
           [queryData.id],
           function (error2, topic) {
@@ -51,7 +51,7 @@ var app = http.createServer(function (request, response) {
             var html = template.HTML(
               title,
               list,
-              `<h2>${title}</h2>${description}`,
+              `<h2>${title}</h2>${description}`, // 입력, 삭제, 수정 폼
               ` <a href="/create">create</a>
                 <a href="/update?id=${queryData.id}">update</a>
                 <form action="delete_process" method="post">
@@ -65,8 +65,8 @@ var app = http.createServer(function (request, response) {
         );
       });
     }
-  } else if (pathname === "/create") {
-    db.query(`SELECT * FROM topic`, function (error, topics) {
+  } else if (pathname === "/create") { // 만들기 버튼을 눌렀을 때
+    db.query(`SELECT * FROM topic`, function (error, topics) { // 쿼리를 통해 읽어오고
       var title = "Create";
       var list = template.list(topics);
       var html = template.HTML(
@@ -88,14 +88,14 @@ var app = http.createServer(function (request, response) {
       response.writeHead(200);
       response.end(html);
     });
-  } else if (pathname === "/create_process") {
+  } else if (pathname === "/create_process") { // 생성 후 처리
     var body = "";
-    request.on("data", function (data) {
+    request.on("data", function (data) { // 데이터를 요청
       body = body + data;
-    });
+    }); 
     request.on("end", function () {
       var post = qs.parse(body);
-      db.query(
+      db.query( // INSERT 구문을 통해 데이터를 MySQL에 저장
         `
             INSERT INTO topic (title, description, created, author_id) 
               VALUES(?, ?, NOW(), ?)`,
@@ -109,22 +109,25 @@ var app = http.createServer(function (request, response) {
         }
       );
     });
-  } else if (pathname === "/update") {
-      db.query(`SELECT * FROM topic`, function(error, topics){
-        if(error) {
-          throw error;
-        }
-        db.query(`SELECT * FROM topic WHERE id = ?`,[queryData.id], function(error2, topic) {
-          if(error2) {
+  } else if (pathname === "/update") { // 수정 버튼을 눌렀을 때
+    db.query("SELECT * FROM topic", function (error, topics) {
+      if (error) {
+        throw error;
+      }
+      db.query(
+        `SELECT * FROM topic WHERE id=?`,
+        [queryData.id],
+        function (error2, topic) {
+          if (error2) {
             throw error2;
           }
           var list = template.list(topics);
           var html = template.HTML(
-          topic[0],
-          list,
-          `
+            topic[0].title,
+            list, // 수정하고자 하는 항목의 제목과 내용을 우선 불러온 다음에 편집할 수 있도록 함
+            `
             <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${topic[0].title}">
+              <input type="hidden" name="id" value="${topic[0].id}">
               <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
               <p>
                 <textarea name="description" placeholder="description">${topic[0].description}</textarea>
@@ -134,37 +137,47 @@ var app = http.createServer(function (request, response) {
               </p>
             </form>
             `,
-          `<a href="/create">create</a> <a href="/update?id=${topic[0].title}">update</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-        });
-      });        
-    }else if (pathname === "/update_process") {
+            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        }
+      );
+    });
+  } else if (pathname === "/update_process") { // 수정 작업 처리
     var body = "";
     request.on("data", function (data) {
       body = body + data;
     });
     request.on("end", function () {
       var post = qs.parse(body);
-      db.query(`UPDATE topic SET title = ?, description = ?, author_id = 1 WHERE id = ?`, [post.title, post.description, post.id], function(error, result){
-        response.writeHead(302, { Location: `/?id=${post.id}` });
-        response.end();
-        })
-      }); 
-  } else if (pathname === "/delete_process") {
+      db.query(
+        `UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?`, // UPDATE 구문을 통해 반영하고 MySQL에 저장
+        [post.title, post.description, post.id],
+        function (error, result) {
+          if (error) {
+            throw error;
+          }
+          response.writeHead(302, { Location: `/?id=${post.id}` });
+          response.end();
+        }
+      );
+    });
+  } else if (pathname === "/delete_process") { // 삭제 처리
     var body = "";
     request.on("data", function (data) {
       body = body + data;
     });
     request.on("end", function () {
       var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function (error) {
+      db.query(`DELETE FROM topic WHERE id = ?`, [post.id], function(error, result){ //DELETE 구문을 통해 삭제 후 MySQL에 반영
+        if (error) {
+          throw error;
+        }
         response.writeHead(302, { Location: `/` });
         response.end();
-      });
+        }
+      );
     });
   } else {
     response.writeHead(404);
